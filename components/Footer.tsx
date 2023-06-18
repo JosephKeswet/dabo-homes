e5bd269eb5fe447cbf750fad4d8cf3c008fd8d1c
JosephKeswet <josephkeswet@gmail.com>
2023-06-18 10:36:28 -0700

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,14 @@ import React from 'react'
 type Props = {}
 
 const Footer = (props: Props) => {
-    const pages = [{routeName:"Home",route:'/'},{routeName:"Contact",route:'/contact'}, {routeName:"About Us",route:'/about'}];
+  const pages = [{title:"Home",route:'/'}, {title:"Contact",route:'/contact'}, {title:"Gallery",route:'/gallery'},{title:"About Us",route:"/about"},{title:"Terms & Conditions",route:'/terms-conditions'}];
+
+    
   return (
     <div>
         <div className='flex justify-center items-center w-full h-[4rem] md:h-[10rem] lg:h-[15rem] bg-[#111111]'>
             <ul className='flex justify-center items-center gap-4 md:gap-20'>
-                {pages?.map((page,index) => (<Link href={page?.route} key={index} className='text-[#DFAA5B] CircularStd text-[0.4375rem] md:text-[1.5rem] lg:text-[2.5rem]'>{page?.routeName}</Link>))}
+                {pages?.map((page,index) => (<Link href={page?.route} key={index} className='text-[#DFAA5B] CircularStd text-[0.4375rem] md:text-[1.5rem] lg:text-[2.5rem]'>{page?.route}</Link>))}
             </ul>
         </div>
     </div>
